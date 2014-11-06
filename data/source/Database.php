331290f75b92d072267fd6170def58808928ecda
Daniel Walton <waltzy+github.com@gmail.com>
2014-11-06 17:45:24 +0000

    Bypasses an error that occurs rarely during the PDO connection construction. Possibly an PHP internal issue.

--- data/source/Database.php
+++ data/source/Database.php
@@ -291,7 +291,10 @@ abstract class Database extends \lithium\data\Source {
 		);
 
 		try {
-			$this->connection = new PDO($dsn, $config['login'], $config['password'], $options);
+			$this->connection = new PDO($dsn, $config['login'], $config['password']);
+			foreach ($options as $name => $value) {
+				$this->connection->setAttribute($name, $value);
+			}
 		} catch (PDOException $e) {
 			preg_match('/SQLSTATE\[(.+?)\]/', $e->getMessage(), $code);
 			$code = $code[1] ?: 0;
